1360dc0ff8de366b75b5ac66bdd6c023cd65bf3f
DenisKolgatin <denissova1453@gmail.com>
2026-06-16 23:48:32 +0300

    first commit

---
+++ src/main/java/com/example/springbootEx2/SpringbootEx2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootEx2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEx2Application.class, args);
+    }
+
+}
